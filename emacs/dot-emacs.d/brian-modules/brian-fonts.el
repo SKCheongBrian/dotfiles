3f453ac69d924c45f23175a6742472b6612743d4
skcheongbrian <skcheongbrian@gmail.com>
2025-02-27 07:24:32 +0800

    fix font for emacs to be bigger on macOS

--- emacs/dot-emacs.d/brian-modules/brian-fonts.el
+++ emacs/dot-emacs.d/brian-modules/brian-fonts.el
@@ -3,8 +3,12 @@
 ;;  mono font. Sans for the rest
 (let ((mono-spaced-font "JetBrainsMono Nerd Font")
       (proportionately-spaced-font "Sans"))
-  (set-face-attribute 'default nil :family mono-spaced-font :height 120)
-  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
-  (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
+  (if (eq system-type "darwin")
+      (progn (set-face-attribute 'default nil :family mono-spaced-font :height 140)
+	     (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.2)
+	     (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.2))
+    (progn (set-face-attribute 'default nil :family mono-spaced-font :height 120)
+	   (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
+	   (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))))
 
 (provide 'brian-fonts)
