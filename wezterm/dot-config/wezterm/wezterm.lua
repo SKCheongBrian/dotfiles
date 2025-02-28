d4968cc4054e8f6c8b1e062b6922256f512c0f40
skcheongbrian <skcheongbrian@gmail.com>
2025-02-28 08:13:37 +0800

    update some things

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ config.font = wezterm.font {
   family = 'JetBrainsMono Nerd Font',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 }
-config.font_size = 20.0
+config.font_size = 16.0
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "carbonfox"
