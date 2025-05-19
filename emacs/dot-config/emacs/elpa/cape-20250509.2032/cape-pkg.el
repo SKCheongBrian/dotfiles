ffac4ae2aaae5a2ae5c21cc93ae2885e3ff59706
skcheongbrian <skcheongbrian@gmail.com>
2025-05-19 21:18:06 +0800

    update

---
+++ emacs/dot-config/emacs/elpa/cape-20250509.2032/cape-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cape" "20250509.2032"
+  "Completion At Point Extensions."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/cape"
+  :commit "4b5be758515cf06cf22de2d1b5de459491f7197b"
+  :revdesc "4b5be758515c"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
--- nvim/dot-config/nvim/lua/plugins/color.lua
+++ nvim/dot-config/nvim/lua/plugins/color.lua
@@ -1,9 +1,17 @@
+-- return {
+--   -- 'mellow-theme/mellow.nvim',
+--   dir = '~/src/banagawa.nvim/',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme('kanagawa-lotus')
+--   end,
+-- }
 return {
-  -- 'mellow-theme/mellow.nvim',
-  dir = '~/src/banagawa.nvim/',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme('kanagawa-lotus')
-  end,
+    'webhooked/kanso.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function ()
+        vim.cmd.colorscheme('kanso-zen')
+    end
 }
