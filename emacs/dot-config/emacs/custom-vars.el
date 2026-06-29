f6272a01d9fbb2bec9868a95dcdf466603b1b015
Brian Sze Kai Cheong <skcheongbrian@gmail.com>
2026-06-29 09:58:37 +0800

    update

---
+++ emacs/dot-config/emacs/custom-vars.el
@@ -0,0 +1,22 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(async auctex avy cape corfu diff-hl diminish doom-modeline eat
+           embark-consult exec-path-from-shell expand-region general
+           go-mode gruvbox-theme haskell-mode helpful hl-todo
+           indent-guide kanagawa-themes latex magit marginalia
+           markdown-mode mason nerd-icons-completion nerd-icons-corfu
+           nerd-icons-dired nerd-icons-ibuffer orderless org-superstar
+           pdf-tools projectile proof-general rainbow-delimiters
+           sideline-flymake toc-org vertico ws-butler yasnippet-capf
+           yasnippet-snippets)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
